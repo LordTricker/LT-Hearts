d8314347522f7ec2bc34e15d4e67bd946c77592b
Oskar Moszczyński <caratoskar3@gazeta.pl>
2025-12-23 13:43:26 +0100

    Initial commit

---
+++ src/client/java/pl/lordtricker/modid/client/config/ConfigLoader.java
@@ -0,0 +1,33 @@
+package pl.lordtricker.modid.client.config;
+
+import net.fabricmc.loader.api.FabricLoader;
+import pl.lordtricker.modid.config.CoreConfigLoader;
+import pl.lordtricker.modid.config.ModConfig;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ConfigLoader {
+    private static final Path MOD_CONFIG_DIR;
+
+    static {
+        Path configDir = FabricLoader.getInstance().getConfigDir();
+        MOD_CONFIG_DIR = configDir.resolve("LT-Mods").resolve("LT-ModTemplate");
+        try {
+            if (!Files.exists(MOD_CONFIG_DIR)) {
+                Files.createDirectories(MOD_CONFIG_DIR);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ModConfig loadConfig() {
+        return CoreConfigLoader.loadConfig(MOD_CONFIG_DIR);
+    }
+
+    public static void saveConfig(ModConfig config) {
+        CoreConfigLoader.saveConfig(config, MOD_CONFIG_DIR);
+    }
+}
---
+++ src/main/java/pl/lordtricker/modid/config/ModConfig.java
@@ -0,0 +1,8 @@
+package pl.lordtricker.modid.config;
+
+public class ModConfig {
+    public boolean adsEnabled = false;
+    public boolean attackDelayTutorEnabled = false;
+    public boolean attackDelayTutorSoundEnabled = true;
+    public boolean attackDelayTutorTextEnabled = true;
+}
